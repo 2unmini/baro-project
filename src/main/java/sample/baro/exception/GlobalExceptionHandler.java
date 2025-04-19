04c4a24782b028a2f5bb26afa92b4d954d44026c
2unmi <2unmin4030@gmail.com>
2025-04-20 02:59:05 +0900

    [Chore] 알 수 없는 서버 에러 핸들러 추가

--- src/main/java/sample/baro/exception/GlobalExceptionHandler.java
+++ src/main/java/sample/baro/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package sample.baro.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sample.baro.dto.response.ExceptionResponse;
 
+import static sample.baro.exception.ErrorCode.INTERNAL_SERVER_ERROR;
 import static sample.baro.exception.ErrorCode.INVALID_CREDENTIALS;
 
 @RestControllerAdvice
@@ -19,5 +21,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleUserCustomException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(e.getStatusCode()).body(ExceptionResponse.of(INVALID_CREDENTIALS));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleUserCustomException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionResponse.of(INTERNAL_SERVER_ERROR));
     }
 }
